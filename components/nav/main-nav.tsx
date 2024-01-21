ba7ffb04f79743613af8503db70bf04d8aa4fbb4
web3crafter <web3crafter@proton.me>
2024-01-21 14:26:10 +0100

    added active indication for nav links

--- components/nav/main-nav.tsx
+++ components/nav/main-nav.tsx
@@ -1,8 +1,12 @@
+"use client"
+
 import Link from "next/link"
 
 import { cn } from "@/lib/utils"
 import { NavItem } from "@/types/nav"
+
 import Logo from "@/components/logo"
+import { usePathname } from "next/navigation"
 
 interface MainNavProps {
   items?: NavItem[]
@@ -10,20 +14,23 @@ interface MainNavProps {
 }
 
 const MainNav = ({ items, className }: MainNavProps) => {
+  const pathName = usePathname()
+
   return (
     <div className="flex gap-6 md:gap-10">
       <Logo />
       {items?.length ? (
         <nav className={cn("flex gap-6", className)}>
           {items.map(
-            (item, index) =>
+            (item) =>
               item.href && (
                 <Link
                   href={item.href}
-                  key={index}
+                  key={item.title}
                   className={cn(
                     "flex items-center text-sm font-medium text-muted-foreground",
-                    item.disabled && "cursor-not-allowed opacity-80"
+                    item.disabled && "cursor-not-allowed opacity-80",
+                    pathName === item.href && "text-foreground"
                   )}
                 >
                   {item.title}
